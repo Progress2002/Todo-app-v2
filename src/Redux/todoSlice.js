5f0e29691cc091a231806e783081668a96b80b7c
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-08-08 11:27:47 +0100

    Setup redux toolkit, store and slice

---
+++ src/Redux/todoSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = JSON.parse(localStorage.getItem('myTodo')) ?? [];
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+      localStorage.setItem('myTodo', JSON.stringify(state));
+    },
+    deleteTodo: (state, action) => {
+      const newState = state.filter((item) => item.id !== action.payload.id);
+      localStorage.setItem('myTodo', JSON.stringify(newState));
+      return newState;
+    },
+    editTodo: (state, action) => {
+      const { id, updateTodo } = action.payload;
+      const index = state.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        state[index].todo = updateTodo;
+        localStorage.setItem('myTodo', JSON.stringify(state));
+      }
+    },
+    toggleComplete: (state, action) => {
+      const { id } = action.payload;
+      const index = state.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        state[index].completed = !state[index].completed;
+        localStorage.setItem('myTodo', JSON.stringify(state));
+      }
+    }
+  },
+});
+
+export const allTodo = (state) => state.todo;
+
+export const { addTodo, deleteTodo, editTodo, toggleComplete } = todoSlice.actions;
+
+export default todoSlice.reducer;
