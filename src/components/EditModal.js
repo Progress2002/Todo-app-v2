137cd1cc58b7dc2d74ad18f603147a4fd50db346
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-08-09 04:35:44 +0100

    Add responsive style to edit modal

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,6 +1,10 @@
 import { useDispatch } from "react-redux"
 import { editTodo } from "../Redux/todoSlice"
 import { useState } from "react";
+import '../styles/EditModal.css'
+import { BsCheck2 } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+
 
 const EditModal = ({ isActive, id, text, closeModal }) => {
   const [editedText, setEditedText] = useState(text);
@@ -21,16 +25,20 @@ const EditModal = ({ isActive, id, text, closeModal }) => {
 
   return (
     <div id='EditModal' className={isActive ? 'active' : ''}>
-      <div>
-        <button onClick={closeModal}>X</button>
-        <p>Edit Message</p>
-      </div>
-      <p>{text}</p>
+      <div className="edit-container">
+        <div className="edit-header">
+          <button onClick={closeModal}><AiOutlineClose /> </button>
+          <h2>Edit Message</h2>
+        </div>
+        <p className="original-text">
+          <span>{text}</span>
+        </p>
 
-      <form onSubmit={handleSubmission}>
-        <input type="text" value={editedText} onChange={handleChange} />
-        <button type="submit"> Edit</button>
-      </form>
+        <form onSubmit={handleSubmission}>
+          <input type="text" value={editedText} onChange={handleChange} />
+          <button type="submit"><BsCheck2 /> </button>
+        </form>
+      </div>
     </div>
   )
 }
