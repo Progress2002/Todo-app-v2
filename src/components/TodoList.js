8dc016e97c506b3de00d7d8f56dcc67ae8c4106a
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-08-09 01:50:29 +0100

    Update TodoList.js file

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,43 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import TodoItem from "./TodoItem"
 import { allTodo } from "../Redux/todoSlice"
+import { useState, useEffect } from "react"
+import { clearCompleted } from "../Redux/todoSlice"
 
 const TodoList = () => {
   const myTodo = useSelector(allTodo)
+  const [todoItems, setTodoItems] = useState(myTodo)
+  const completed = myTodo.filter((todo) => todo.completed === true);
+  const active = myTodo.filter((todo) => todo.completed === false);
+  const handleCompleted = () => setTodoItems(completed);
+  const handleActive = () => setTodoItems(active);
+  const handleAllTodo = () => setTodoItems(myTodo);
+
+  const dispatch = useDispatch();
+
+  const handleClearCompleted = () => dispatch(clearCompleted())
+
+  useEffect(()=> {
+    setTodoItems(myTodo)
+  }, [myTodo])
   return (
-    <ul>
-      {
-        myTodo.map((todo) => (
-          <TodoItem id={todo.id} key={todo.id} isCompleted={todo.completed} text={todo.todo}/>
-        ))
-      }
-    </ul>
+    <div>
+      <ul>
+        {
+          todoItems.map((item) => (
+            <TodoItem id={item.id} key={item.id} isCompleted={item.completed} text={item.todo}/>
+          ))
+        }
+      </ul>
+      <div>
+        <p>
+          {
+            active.length > 1 ? `${active.length} items left` : `${active.length} item left`
+          }
+        </p>
+        <button type="button" onClick={handleClearCompleted}>Clear completed</button>
+      </div>
+    </div>
   )
 }
 
